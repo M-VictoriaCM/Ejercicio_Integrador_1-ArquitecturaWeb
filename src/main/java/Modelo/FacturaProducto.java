0044285f86b0214e04e458a32321207fdc7a1410
M Victoria <mvittoria7@gmail.com>
2023-08-30 02:40:16 -0300

    Cambios en la clases factura

--- src/main/java/Modelo/FacturaProducto.java
+++ src/main/java/Modelo/FacturaProducto.java
@@ -1,25 +1,25 @@
 package Modelo;
 
 public class FacturaProducto {
-    private int idFactura;
+    private Factura idFactura;
     private Producto idProducto;
     private int cantidad;
 
-    public FacturaProducto(int idFactura, Producto idProducto, int cantidad) {
+    public FacturaProducto(Factura idFactura, Producto idProducto, int cantidad) {
         this.idFactura = idFactura;
         this.idProducto = idProducto;
         this.cantidad = cantidad;
     }
+    public FacturaProducto(int cantidad) {
+        this.cantidad = cantidad;
+    }
 
-    public int getIdFactura() {
+    public Factura getIdFactura() {
         return idFactura;
     }
+
     public Producto getIdProducto() {
         return idProducto;
-    }
-
-    public void setIdProducto(Producto idProducto) {
-        this.idProducto = idProducto;
     }
 
     public int getCantidad() {
@@ -28,14 +28,5 @@ public class FacturaProducto {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
-    }
-
-    @Override
-    public String toString() {
-        return "FacturaProducto{" +
-                "id=" + idFactura +
-                ", idProducto=" + idProducto +
-                ", cantidad=" + cantidad +
-                '}';
     }
 }
